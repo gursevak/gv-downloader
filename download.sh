855314a48aba284c69803175f3df0f1f1b940b2f
Irvanjit Singh <1472548+irvanjitsingh@users.noreply.github.com>
2019-02-11 02:36:47 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 FILENAME=playlist.m3u
 LINECOUNT=$(wc -l < "$FILENAME")
-local -i COUNTER=0
+COUNTER=0
 echo "There are $LINECOUNT audio tracks in this playlist."
 read -p "Are you sure you want to download them all? [Y/n]" -n 1 -r
 echo
